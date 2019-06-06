c381aaa5ad1d487423583dcef6cba3f1873c9f5f
zonghui <tj_zhui@126.com>
2019-06-07 01:58:26 +0800

    update gc code

--- scripts/rosalind_gc.py
+++ scripts/rosalind_gc.py
@@ -1,17 +1,29 @@
-Python 2.7.8 (default, Jun 30 2014, 16:03:49) [MSC v.1500 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> fp=open('C:\\Users\\sony\\Desktop\\rosalind_gc.txt','r')
->>> str=fp.read()
->>> list=str.split('>')
->>> del list[0]
->>> dict={}
->>> for i in range(len(list)):
-	dict[list[i][0:13]]=float(list[i].count('C')+list[i].count('G'))/(list[i].count('A')+list[i].count('T')+list[i].count('C')+list[i].count('G'))
+# python3
 
 
-	
->>> 
-KeyboardInterrupt
->>> print sorted(dict.items(), key=lambda d: d[1])
-[('Rosalind_5573', 0.4962871287128713), ('Rosalind_4480', 0.5028636884306987), ('Rosalind_8391', 0.5062571103526735), ('Rosalind_8944', 0.5069605568445475), ('Rosalind_8470', 0.5096153846153846)]
->>> 
+# Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
+# Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. Rosalind allows for a default error of 0.001 in all decimal answers unless otherwise stated; please see the note on absolute error below.
+
+def gc(file):
+	gc_contents = {}
+	strings = {}
+	with open(file, 'r') as f:
+		for line in f:
+			line = line.strip()
+			if line.startswith(">"):
+				id = line.replace(">", "")
+				strings[id] = ""
+			else:
+				strings[id] += line
+	for k in strings.keys():
+		gc_content = (strings[k].count("G") + strings[k].count("C")) / float(len(strings[k]))
+		gc_contents[k] = gc_content
+	gc_contents = sorted(gc_contents.items(), key=lambda d: d[1], reverse = True)
+	highest_gc = gc_contents[0]
+	return highest_gc
+
+
+if __name__ == "__main__":
+	highest_gc = gc("../data/rosalind_gc.txt")
+	print(highest_gc[0])
+	print(highest_gc[1]*100)
