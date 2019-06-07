e2451ab86301135ee8e5a0f290f925e6fd1a02bf
zonghui <tj_zhui@126.com>
2019-06-07 10:42:41 +0800

    update hamm code

--- scripts/rosalind_hamm.py
+++ scripts/rosalind_hamm.py
@@ -1,22 +1,23 @@
-# coding:utf-8
-# Counting Point Mutations
+# python3
 
-# Sample Dataset
-# GAGCCTACTAACGGGAT
-# CATCGTAATGACGGCCT
 
-# Sample Output
-#7
+# Given: Two DNA strings  and  of equal length (not exceeding 1 kbp).
+# Return: The Hamming distance
 
-def hamming_distance(file):
-	fp = open(file, 'r')
-	seq_list = []
-	distances = 0
-	for line in fp:
-		seq_list.append(line.replace('\n', ''))
-	for i in range(len(seq_list[0])):
-		if seq_list[0][i] != seq_list[1][i]:
-			distances += 1
-	print distances
 
-# hamming_distance("rosalind_hamm.txt")
+def hamm(string1, string2):
+	distance = 0
+	if len(string1) == len(string2):
+		for i in range(len(string1)):
+			if string1[i] != string2[i]:
+				distance += 1
+	return distance
+
+
+if __name__ == "__main__":
+	with open("../data/rosalind_hamm.txt", "r") as f:
+		string1 = f.readline().strip()
+		string2 = f.readline().strip()
+		print(hamm(string1, string2))
+
+
