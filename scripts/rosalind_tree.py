a9216b5dbdc0575232cf75aef4185ba1c5425900
zonghui0228 <1632755@tongji.edu.cn>
2019-12-25 14:49:49 +0800

    update

--- scripts/rosalind_tree.py
+++ scripts/rosalind_tree.py
@@ -1,32 +1,49 @@
+# ^_^ coding:utf-8 ^_^
+
 # completing a tree
-# the minimum number of edges that can be added to the graph to produce a tree
-
-def minimumtree(n, adlist):
-	edges = []
-	m = 0
-	while len(adlist) > 0:
-		edges.append(adlist[0])
-		del adlist[0]
-		for i in adlist:
-			for j in i:
-				if j in edges[m]:
-					edges[m] += i
-					adlist.remove(i)
-					break
-		m += 1
-	
-	edges = [set(edge) for edge in edges]
-	c = 0
-	for edge in edges:
-		c += len(edge)
-
-	# print edges
-	return n - c + len(edges) - 1
 
 
-if __name__ == '__main__':
-	f = open('test.txt','r')
-	n = int(f.readline())
-	l = [line.strip().split(' ') for line in f.readlines()]
-	f.close()
-	print minimumtree(n, l)
+# ==============================
+# config the input
+data = "../data/rosalind_tree.txt"
+
+
+
+# ==============================
+# 1. read data
+with open(data, "r") as f:
+    n = int(f.readline())
+    adjacency_list = [line.strip().split(" ") for line in f]
+# print(n)
+# print(adjacency_list)
+
+# 2. get minimum number of edges that can be added to the graph to produce a tree.
+subtrees = [] # 能连接在一起的树
+nodes = set() # 已经有边的点
+for i, j in adjacency_list:
+    if i in nodes or j in nodes:
+        for st in subtrees:
+            if i in st or j in st:
+                subtrees[subtrees.index(st)].append(i)
+                subtrees[subtrees.index(st)].append(j)
+                nodes.add(i), nodes.add(j)
+    else:
+        subtrees.append([i,j])
+        nodes.add(i), nodes.add(j)
+
+# 3. 合并subtrees中具有相同元素的列表
+l = len(subtrees)
+for i in range(l):
+	for j in range(l):
+		if i==j:
+			break
+		if len(set(subtrees[i] + subtrees[j])) < len(subtrees[i]) + len(subtrees[j]):
+			subtrees[i] = list(set(subtrees[i] + subtrees[j]))
+			subtrees[j] = []
+subtrees = [i for i in subtrees if i]
+# print(subtrees)
+# print(nodes)
+
+
+result = (n -len(nodes)) + len(subtrees) - 1
+print(result)
