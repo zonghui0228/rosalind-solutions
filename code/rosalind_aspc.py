9bab35581cfa5a0e6c6b08b6f08c835e6baf93d5
zonghui0228 <1632755@tongji.edu.cn>
2020-07-20 15:37:06 +0800

    update

--- code/rosalind_aspc.py
+++ code/rosalind_aspc.py
@@ -6,13 +6,13 @@ import math
 
 # load data
 with open("../data/rosalind_aspc.txt", "r") as f:
-	n, m = map(int, f.readline().strip().split(" "))
+    n, m = map(int, f.readline().strip().split(" "))
 print("Positive integers n and  m are: {}, {}".format(n,m))
 
 # The sum of combinations C(n,k) for all k satisfying m≤k≤n, modulo 1,000,000.
 c = 0
 for k in range(m, n+1):
-	c += math.factorial(n) // (math.factorial(k) * math.factorial(n-k))
+    c += math.factorial(n) // (math.factorial(k) * math.factorial(n-k))
 
 print("The sum of combinations: {}".format(c))
 print("The sum of combinations (modulo 1,000,000): {}".format(c%1000000))
